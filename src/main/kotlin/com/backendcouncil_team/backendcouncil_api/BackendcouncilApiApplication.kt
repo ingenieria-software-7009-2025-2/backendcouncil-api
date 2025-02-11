9f99f468121e3651ab7c7d02388148c967221d0d
MarianaBernal <mariana_bernal@ciencias.unam.mx>
2025-02-11 16:44:17 -0600

    first commit

---
+++ src/main/kotlin/com/backendcouncil_team/backendcouncil_api/BackendcouncilApiApplication.kt
@@ -0,0 +1,11 @@
+package com.backendcouncil_team.backendcouncil_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendcouncilApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendcouncilApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/backendcouncil_team/backendcouncil_api/BackendcouncilApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backendcouncil_team.backendcouncil_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendcouncilApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
