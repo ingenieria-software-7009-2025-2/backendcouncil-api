bcc5a333adc9d99581a6fb5765025a79814b8e38
ccwebi <isaacrobled@ciencias.unam.mx>
2025-02-22 18:41:08 -0600

    Documented. Updated dirs.
    Add documentation KDoc style.
    Add users dir to organize classes.
    Deleted HealthController (so HealthController and Mascota's classes are not in the project).
    Update classes packages.
    Update datain functions.
    Pending check.

--- src/main/kotlin/com/backendcouncil_team/backendcouncil_api/Usuario.kt
+++
@@ -1,8 +0,0 @@
-package com.backendcouncil_team.backendcouncil_api
-
-class Usuario(
-    val name : String = "",
-    val mail : String = "" ,
-    val password : String = "" ,
-    val token : String = "" ,
-) {}
